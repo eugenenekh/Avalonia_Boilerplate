7cf6b26dad426916ff8dbca06665e720d34fa1a8
Aleksey Ermolinsky <aleksey.ermalinsky@outsystems.onmicrosoft.com>
2021-03-12 08:30:21 +0000

    add hidden window and listen for closing

--- Avalonia.Boilerplate/Avalonia.Boilerplate/DialogWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/DialogWindow.xaml.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Threading.Tasks;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia.Boilerplate {
     public class DialogWindow : Window {
+
+        private static WindowBase hiddenWindow;
+
         public DialogWindow() {
             InitializeComponent();
+            GetHiddenWindow();
         }
 
         public Task<bool> ShowModalWindow(Window owner) {
@@ -17,6 +21,22 @@ namespace Avalonia.Boilerplate {
             AvaloniaXamlLoader.Load(this);
             Width = 300;
             Height = 300;
+        }
+
+        private static WindowBase GetHiddenWindow() {
+            if (hiddenWindow == null) {
+                hiddenWindow = new Window() {
+                    IsVisible = false,
+                    Focusable = false,
+                    Title = "Hidden React View Window"
+                };
+                hiddenWindow.Closed += new EventHandler(OnHiddenWindowClose);
+            }            
+            return hiddenWindow;
+        }
+
+        private static void OnHiddenWindowClose(object sender, EventArgs e) {
+            return;
         }
     }
 }
