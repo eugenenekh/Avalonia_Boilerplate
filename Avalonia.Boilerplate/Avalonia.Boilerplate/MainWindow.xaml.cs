b91aee34c6ae608df124434a195d9aa03331393e
João Neves <joao.neves@outsystems.com>
2021-04-13 12:26:33 +0100

    Fullscreen repro

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia.Boilerplate {
@@ -13,6 +15,27 @@ namespace Avalonia.Boilerplate {
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private Window popup;
+        
+        private void InputElement_OnPointerEnter(object? sender, PointerEventArgs e)
+        {
+            if (popup == null)
+            {
+                popup = new Window();
+                popup.ShowActivated = false;
+                popup.Width = 100;
+                popup.Height = 100;
+                popup.Position = new PixelPoint(500, 200);
+            }
+
+            popup.Show();
+        }
+
+        private void InputElement_OnPointerLeave(object? sender, PointerEventArgs e)
+        {
+            popup.Hide();
         }
     }
 }
