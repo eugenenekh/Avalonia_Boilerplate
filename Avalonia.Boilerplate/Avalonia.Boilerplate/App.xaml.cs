89b377f1757091f915ecafd0d903ea991b832087
João Neves <joao.neves@outsystems.com>
2021-04-13 15:38:39 +0100

    fixed position

--- Avalonia.Boilerplate/Avalonia.Boilerplate/App.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/App.xaml.cs
@@ -11,6 +11,7 @@ namespace Avalonia.Boilerplate {
         public override void OnFrameworkInitializationCompleted() {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
                 desktop.MainWindow = new MainWindow();
+                new MainWindow().Show();
             }
 
             base.OnFrameworkInitializationCompleted();
--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -6,36 +6,40 @@ using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia.Boilerplate {
-    public class MainWindow : Window {
+    public class MainWindow : Window
+    {
+
+        private static Window popup;
+        
         public MainWindow() {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
-            
-            popup = new Window();
-            popup.Focusable = false;
-            popup.ShowActivated = false;
-            popup.Width = 100;
-            popup.Height = 100;
-            popup.Position = new PixelPoint(500, 200);
-            popup.Show();
-            popup.Hide();
         }
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
         }
 
-        private Window popup;
-        
         private void InputElement_OnPointerEnter(object? sender, PointerEventArgs e)
         {
-            popup.Show();
+            if (popup == null)
+            {
+                popup = new Window();
+                popup.ShowActivated = false;
+                popup.Width = 100;
+                popup.Height = 100;
+                popup.Content = "Tooltip";
+            }
+            popup.Position = new PixelPoint(Position.X + 10, Position.Y + 20);
+            popup.Show(this);
         }
 
         private void InputElement_OnPointerLeave(object? sender, PointerEventArgs e)
         {
+            popup.Hide();
+            popup.Show();
             popup.Hide();
         }
     }
