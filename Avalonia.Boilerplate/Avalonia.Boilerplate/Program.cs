142726728b78221921a61351a963bbd0d030071e
João Malés <joao.males@outsystems.com>
2021-04-28 09:58:35 +0100

    Setup example.

--- Avalonia.Boilerplate/Avalonia.Boilerplate/Program.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/Program.cs
@@ -1,20 +1,63 @@
 using System;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
+using Microsoft.Win32;
 
 namespace Avalonia.Boilerplate {
     class Program {
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
+        public static void Main(string[] args) {
+
+            //Add to Windows registry
+            try {
+                AddToRegistry();
+            } catch(Exception e) {
+                Console.WriteLine(e);
+            }
+            
+            BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
+
+            Application.Current.UrlsOpened += (object sender, UrlOpenedEventArgs e) => {
+                System.Diagnostics.Debugger.Launch();
+            };
+        }
+
+        private void UrlsOpened(object sender, UrlOpenedEventArgs e) {
+            System.Diagnostics.Debugger.Launch();
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToTrace();
+
+        private static void AddToRegistry() {
+
+#pragma warning disable CA1416 // Validate platform compatibility
+            RegistryKey existingKey = Registry.ClassesRoot.OpenSubKey("AvaloniaBoilerplate", true);
+            if (existingKey != null) {
+                return;
+            }
+
+            RegistryKey key = Registry.ClassesRoot.CreateSubKey("AvaloniaBoilerplate");
+            key.SetValue("", "URL:AvaloniaBoilerplate Protocol");
+            key.SetValue("URL Protocol", "");
+
+            key.CreateSubKey("shell");
+            key = key.OpenSubKey("shell", true);
+
+            key?.CreateSubKey("open");
+            key = key.OpenSubKey("open", true);
+
+            key?.CreateSubKey("command");
+            key = key.OpenSubKey("command", true);
+
+            var exeDir = AppDomain.CurrentDomain.BaseDirectory + "Avalonia.Boilerplate.exe";
+            key?.SetValue("", exeDir);
+#pragma warning restore CA1416 // Validate platform compatibility
+        }
     }
 }
