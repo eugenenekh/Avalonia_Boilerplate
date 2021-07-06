65d09af587756a97638f63320f0c2c6842bbd419
João Neves <joao.neves@outsytems.com>
2021-07-06 11:34:26 +0100

    Red window bug

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
@@ -13,6 +16,21 @@ namespace Avalonia.Boilerplate {
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+            var w = new Window();
+            var w2 = new Window();
+            w2.Title = "w2";
+            w2.MinWidth = 200;
+            w2.MinHeight = 200;
+            w2.Show();
+            w2.Hide();
+            w.Title = "w";
+            w.Show(w2);
+            
         }
     }
 }
