93f99fc5a36aac8baaee172125b43ca1d4f7d592
tfs <tiago.fernandes@outsystems.com>
2021-07-09 15:08:26 +0100

    Reproduce issue with avalonia shell after opening context menu

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,16 +1,40 @@
-using Avalonia;
+using System.Collections;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
+        private ContextMenu contextMenu;
+
         public MainWindow() {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+            
+            AddHandler(PointerReleasedEvent, MouseUpHandler, handledEventsToo: true);
         }
 
+        private void MouseUpHandler(object? sender, PointerReleasedEventArgs e)
+        {
+            contextMenu?.Close();
+            contextMenu = null;
+            
+            if (e.InitialPressMouseButton == MouseButton.Left)
+            {
+                return;
+            } 
+            
+            contextMenu = new ContextMenu();
+            contextMenu.Items = new ArrayList
+            {
+                new ItemsControl() { Name = "entry 1" }
+            };
+
+            contextMenu.Open(this);
+        }
+        
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
         }
