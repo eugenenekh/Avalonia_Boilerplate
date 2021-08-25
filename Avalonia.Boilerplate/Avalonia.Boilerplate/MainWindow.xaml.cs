ab70bd1bb69163f6eda5d1b8bad4cecc4ffcd270
Evgeny Nehaychik <evgeny.nehaychik@outsystems.com>
2021-08-25 17:02:56 +0100

    Update versions

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,13 +1,22 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using WebViewControl;
 
 namespace Avalonia.Boilerplate {
     public class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+
+            WebView.Settings.OsrEnabled = false;
+
+            var webview = new WebView();
+            webview.Address = "https://google.com";
+
+            this.Content = webview;
         }
 
         private void InitializeComponent() {
